56dcea6bff9b929dd5436feb74a7683fb1e74567
haryaprianto <16022596@myrp.edu.sg>
2018-04-30 14:35:01 +0800

    //Done 2

--- app/src/main/java/com/myapplicationdev/android/p03classjournal/InfoModule.java
+++ app/src/main/java/com/myapplicationdev/android/p03classjournal/InfoModule.java
@@ -1,13 +1,32 @@
 package com.myapplicationdev.android.p03classjournal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class InfoModule extends AppCompatActivity {
+    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_module);
+
+        btnAdd = (Button)findViewById(R.id.buttonAdd);
+
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InfoModule.this, Info.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 }
