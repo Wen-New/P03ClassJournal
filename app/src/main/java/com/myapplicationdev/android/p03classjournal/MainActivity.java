7e33446f39bd2d2f9c3cb1aa1eba0771d950ed0e
haryaprianto <16022596@myrp.edu.sg>
2018-04-30 13:18:34 +0800

    //FIrst two page done.

--- app/src/main/java/com/myapplicationdev/android/p03classjournal/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p03classjournal/MainActivity.java
@@ -1,13 +1,48 @@
 package com.myapplicationdev.android.p03classjournal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    ListView lvModule;
+    ArrayList<String> arrayListModule;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvModule = (ListView)findViewById(R.id.lvModule);
+
+        arrayListModule = new ArrayList<String>();
+        arrayListModule.add("C302");
+
+        ArrayAdapter arrayAdapterHoliday = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayListModule);
+
+        lvModule.setAdapter(arrayAdapterHoliday);
+
+
+        lvModule.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String title = arrayListModule.get(position);
+
+                Intent intent = new Intent(MainActivity.this, InfoModule.class);
+                intent.putExtra("title", title);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
