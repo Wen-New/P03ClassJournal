0aef388c7e48aede1efc0fd8b53913e3b986f0b8
16046544 <16046544@myrp.edu.sg>
2018-04-30 14:37:13 +0800

    //info web

--- app/src/main/java/com/myapplicationdev/android/p03classjournal/InfoModule.java
+++ app/src/main/java/com/myapplicationdev/android/p03classjournal/InfoModule.java
@@ -1,13 +1,31 @@
 package com.myapplicationdev.android.p03classjournal;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InfoModule extends AppCompatActivity {
 
+    Button btnInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_module);
+
+        btnInfo = (Button) findViewById(R.id.buttonInfo);
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // Intent to display data
+                Intent rpIntent = new Intent(Intent.ACTION_VIEW);
+                // Set the URL to be used.
+                rpIntent.setData(Uri.parse("http://www.rp.edu.sg"));
+                startActivity(rpIntent);
+            }
+        });
+
     }
 }
