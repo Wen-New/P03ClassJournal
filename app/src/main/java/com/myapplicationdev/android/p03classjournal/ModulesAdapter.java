358f00c1b73ddc0c17c75a10198add85f9b61686
haryaprianto <16022596@myrp.edu.sg>
2018-04-30 14:38:18 +0800

    //Done 2

--- app/src/main/java/com/myapplicationdev/android/p03classjournal/ModulesAdapter.java
+++ app/src/main/java/com/myapplicationdev/android/p03classjournal/ModulesAdapter.java
@@ -2,5 +2,5 @@ package com.myapplicationdev.android.p03classjournal;
 
 import android.widget.ArrayAdapter;
 
-public class ModulesAdapter extends ArrayAdapter {
-}
+//public class ModulesAdapter extends ArrayAdapter {
+//}
